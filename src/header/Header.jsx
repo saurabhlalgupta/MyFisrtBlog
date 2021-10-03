d2b3eeddb5dfdc035b72219282535b096673b576
luckylalgupta <lucky.gupta@airtelbank.com>
2021-10-03 10:04:01 +0530

    Header components

---
+++ src/header/Header.jsx
@@ -0,0 +1,15 @@
+import "./header.css"
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className="headerTitles">
+                <span className="headerTitleSm">React  &  Node</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img 
+            className="headerImg"
+            src="https://images.pexels.com/photos/3265460/pexels-photo-3265460.jpeg?cs=srgb&dl=pexels-stijn-dijkstra-3265460.jpg&fm=jpg" alt="" />
+        </div>
+    )
+}
