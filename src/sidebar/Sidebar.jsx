c6b32873fcd369f8728a4234d3a1a64d0b74eba5
luckylalgupta <lucky.gupta@airtelbank.com>
2021-10-08 22:22:15 +0530

    Added sidebar and about me

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                 <span className="sidebarTitle">
+                  ABOUT ME   
+                 </span>
+                 <img className ="sidebarImg" src="https://images.pexels.com/photos/4995761/pexels-photo-4995761.jpeg?cs=srgb&dl=pexels-tima-miroshnichenko-4995761.jpg&fm=jpg" alt="" />
+                 <p>
+                     Loren ipsum dolar sit amet consectetur adipisicing elit. Voluptate qui necessitatibus nostrum illum reprehenderit.
+                 </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTilte">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="topIcon fab fa-facebook"></i>
+                    <i className="topIcon fab fa-twitter-square"></i>
+                    <i className="topIcon fab fa-pinterest"></i>
+                    <i className="topIcon fab fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
